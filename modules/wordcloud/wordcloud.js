a492115575c831a9a64c3533ec7803b4cfc0141c
etonxonline <123908790+etonxonline@users.noreply.github.com>
2023-04-09 00:24:31 +0100

    Update wordcloud.js

--- modules/wordcloud/wordcloud.js
+++ modules/wordcloud/wordcloud.js
@@ -13,7 +13,7 @@ if (params.hide != null)
 var words = [];
 
 $(document).ready(function(){
-	console.log("Ready now")
+	console.log("Ready now 2")
 	var wordcloudInput = $('#wordcloudInput');
 	if (wordcloudInput !=  null) {
 		printWordcloudInput(wordcloudInput);
